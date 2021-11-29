4bfb3f0c947172b5f7a787000441b23426a5ea06
bahreingiri <bahrein.giri@gmail.com>
2021-11-29 13:27:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <title>My Portofolio</title>
+  </head>
+  <body>
+
+  <div class="header-bar">
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #030303d7;">
+        <div class="container">
+                <a class="navbar-brand" style="color: white; font-weight: bold;" href="#">Bahrein Giri Fillahi A'rof</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" style="color: white;" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: white;" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: white;" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: white;" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="main-website">
+        <div class="main-1">
+            <div class="jumbotron text-center" style="background-color: #030303c5; padding-top: 10px;">
+                <img src="19650077.png" class="rounded-circle img-thumbnail" style="width: 200px; height: 200px;" alt="foto profil">
+                <h1 class="display-5" style="color: white;">Bahrein Giri Fillahi A'rof (19650077)</h1>
+                <p class="lead" style="color: white;">Web Developer | Cyber Security</p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#949090" 
+                fill-opacity="1" d="M0,256L0,96L60,96L60,192L120,192L120,64L180,64L180,0L240,0L240,0L300,0L300,192L360,192L360,256L420,256L420,224L480,224L480,128L540,128L540,64L600,64L600,224L660,224L660,32L720,32L720,224L780,224L780,32L840,32L840,224L900,224L900,64L960,64L960,128L1020,128L1020,256L1080,256L1080,160L1140,160L1140,320L1200,320L1200,256L1260,256L1260,256L1320,256L1320,64L1380,64L1380,0L1440,0L1440,320L1380,320L1380,320L1320,320L1320,320L1260,320L1260,320L1200,320L1200,320L1140,320L1140,320L1080,320L1080,320L1020,320L1020,320L960,320L960,320L900,320L900,320L840,320L840,320L780,320L780,320L720,320L720,320L660,320L660,320L600,320L600,320L540,320L540,320L480,320L480,320L420,320L420,320L360,320L360,320L300,320L300,320L240,320L240,320L180,320L180,320L120,320L120,320L60,320L60,320L0,320L0,320Z"></path></svg>
+            </div>
+        </div>
+        <div class="main-2">
+            <div class="jumbotron" style="background-color: #949090;">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col"> <h1 style="color: white;">About Me</h1></div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col text-center">
+                        <img src="Mi.jpg" class="rounded img-thumbnail" alt="foto MI" style="width: 300px; height: 200px;">
+                        <p style="color: white; font-weight: bold;">MI Attaraqqie (2007-2013)</p>
+                        </div>
+                        <div class="col text-center">
+                        <img src="Mts.jpg" class="rounded img-thumbnail" alt="foto MTS" style="width: 300px; height: 200px;">
+                        <p style="color: white; font-weight: bold;">MTsN Malang III (2013-2016)</p>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col text-center">
+                        <img src="Ma.jfif" class="rounded img-thumbnail" alt="foto MA" style="width: 300px; height: 200px;">
+                        <p style="color: white; font-weight: bold;">MAN Gondanglegi (2016-2019)</p>
+                        </div>
+                        <div class="col text-center">
+                        <img src="Uin.jpg" class="rounded img-thumbnail" alt="foto UIN" style="width: 300px; height: 200px;">
+                        <p style="color: white; font-weight: bold;">UIN Maulana Malik Ibrahim (2019-sekarang)</p>
+                        </div>
+                    </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#181717" fill-opacity="1" 
+                    d="M0,224L8.6,229.3C17.1,235,34,245,51,208C68.6,171,86,85,103,
+                    64C120,43,137,85,154,133.3C171.4,181,189,235,206,224C222.9,213,
+                    240,139,257,90.7C274.3,43,291,21,309,32C325.7,43,343,85,360,112C377.1,
+                    139,394,149,411,144C428.6,139,446,117,463,96C480,75,497,53,514,80C531.4,
+                    107,549,181,566,208C582.9,235,600,213,617,202.7C634.3,192,651,192,669,165.3C685.7,
+                    139,703,85,720,101.3C737.1,117,754,203,771,224C788.6,245,806,203,823,192C840,181,857,203,
+                    874,197.3C891.4,192,909,160,926,170.7C942.9,181,960,235,977,245.3C994.3,256,1011,224,1029,192C1045.7,160,1063,128,1080,
+                    122.7C1097.1,117,1114,139,1131,160C1148.6,181,1166,203,1183,181.3C1200,160,1217,96,1234,106.7C1251.4,
+                    117,1269,203,1286,234.7C1302.9,267,1320,245,1337,208C1354.3,171,1371,117,1389,122.7C1405.7,128,1423,192,1431,224L1440,256L1440,320L1431.4,320C1422.9,320,1406,320,1389,320C1371.4,320,1354,320,1337,320C1320,320,1303,320,1286,320C1268.6,320,1251,320,1234,320C1217.1,320,1200,320,1183,320C1165.7,320,1149,320,1131,320C1114.3,320,1097,320,1080,320C1062.9,320,1046,320,1029,320C1011.4,320,994,320,977,320C960,320,943,320,926,320C908.6,320,891,320,874,320C857.1,320,840,320,823,320C805.7,320,789,320,771,320C754.3,320,737,320,720,320C702.9,320,686,320,669,320C651.4,320,634,320,617,320C600,320,583,320,566,320C548.6,320,531,320,514,320C497.1,320,480,320,463,320C445.7,320,429,320,411,320C394.3,320,377,320,360,320C342.9,320,326,320,309,320C291.4,320,274,320,257,320C240,320,223,320,206,320C188.6,320,171,320,154,320C137.1,320,120,320,103,320C85.7,320,69,320,51,320C34.3,320,17,320,9,320L0,320Z"></path>
+                </svg>
+            </div>
+        </div>
+        <div class="main-3">
+            <div class="jumbotron" style="background-color: #181717;">
+                <div class="container">
+                <div class="row">
+                    <div class="col"><h1 class="text-center" style="color: white;">My Projects</h1></div>
+                </div>
+                <div class="row justify-content-center" style="padding: 10px;">
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="P1.png" class="card-img-top" alt="project1">
+                            <div class="card-body">
+                                <h5 class="card-title">Project 1</h5>
+                                <p class="card-text">Membuat Website Responsif Aplikasi Fitur Progate</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="P2.png" class="card-img-top" alt="project2">
+                            <div class="card-body">
+                                <h5 class="card-title">Project 2</h5>
+                                <p class="card-text">Membuat Website Responsif Aplikasi Pengelola Content</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center" style="padding: 10px;">
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="P3.png" class="card-img-top" alt="project3">
+                            <div class="card-body">
+                                <h5 class="card-title">Project 3</h5>
+                                <p class="card-text">Membuat Rangkaian Elektronika Project Smart Detector Movement</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="P4.png" class="card-img-top" alt="project4">
+                            <div class="card-body">
+                                <h5 class="card-title">Project 4</h5>
+                                <p class="card-text">Membuat Rangkaian Susunan Jaringan Komputer Pada Fakultas Saintek</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#adadadd3" fill-opacity="1" 
+                d="M0,288L0,64L80,64L80,64L160,64L160,160L240,160L240,192L320,192L320,128L400,
+                128L400,0L480,0L480,288L560,288L560,32L640,32L640,32L720,32L720,192L800,192L800,
+                32L880,32L880,96L960,96L960,96L1040,96L1040,64L1120,64L1120,64L1200,64L1200,256L1280,
+                256L1280,96L1360,96L1360,224L1440,224L1440,320L1360,320L1360,320L1280,320L1280,320L1200,
+                320L1200,320L1120,320L1120,320L1040,320L1040,320L960,320L960,320L880,320L880,320L800,320L800,
+                320L720,320L720,320L640,320L640,320L560,320L560,320L480,320L480,320L400,320L400,320L320,
+                320L320,320L240,320L240,320L160,320L160,320L80,320L80,320L0,320L0,320Z"></path>
+                </svg>
+            </div>
+        </div>
+        <div class="main-4">
+            <div class="jumbotron" style="background-color: #adadadd3;">
+                <div class="container">
+                    <div class="row text-center md-3">
+                        <div class="col"><h1 class="text-center" style="color: rgb(15, 15, 15);">Contact Me</h1></div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-6">
+                            <form name="my-contact-form">
+                                <div class="mb-3">
+                                    <label for="nama" class="form-label">Nama</label>
+                                    <input type="text" class="form-control" id="nama" aria-describedby="name" name="nama">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="email" aria-describedby="email" name="email">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="pesan" class="form-label">Pesan</label>
+                                    <textarea class="form-control" id="pesan" rows="3" name="pesan"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-dark">Kirim</button>
+                                <button class="btn btn-dark d-none" type="button" disabled>
+                                    <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+                                    Loading...
+                                </button>
+                            </form>
+                            <div class="alert alert-success alert-dismissible fade show d-none" role="alert" style="margin-top: 20px;">
+                                <strong>Terima Kasih :)</strong> Anda Telah Berhasil Mengirim Pesan....
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                            <script>
+                                const scriptURL = 'https://script.google.com/macros/s/AKfycbyGzVmplllbTbsI-cdtT_gKHs80p1wzQeHRJLSCFQD99ejA1Rg-K5wLqDsdfoqnYjNg/exec'
+                                const form = document.forms['my-contact-form']
+
+                                form.addEventListener('submit', e => {
+                                    e.preventDefault()
+                                    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                                    .then(response => console.log('Success!', response))
+                                    .catch(error => console.error('Error!', error.message))
+                                })
+                            </script>
+                        </div>
+                    </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000000" 
+                fill-opacity="1" d="M0,32L13.3,48C26.7,64,53,96,80,128C106.7,160,133,192,160,202.7C186.7,
+                213,213,203,240,170.7C266.7,139,293,85,320,90.7C346.7,96,373,160,400,197.3C426.7,
+                235,453,245,480,224C506.7,203,533,149,560,154.7C586.7,160,613,224,640,213.3C666.7,203,693,
+                117,720,96C746.7,75,773,117,800,154.7C826.7,192,853,224,880,208C906.7,192,933,128,960,
+                128C986.7,128,1013,192,1040,186.7C1066.7,181,1093,107,1120,106.7C1146.7,107,1173,181,1200,
+                181.3C1226.7,181,1253,107,1280,117.3C1306.7,128,1333,224,1360,240C1386.7,256,1413,192,1427,
+                160L1440,128L1440,320L1426.7,320C1413.3,320,1387,320,1360,320C1333.3,320,1307,320,1280,
+                320C1253.3,320,1227,320,1200,320C1173.3,320,1147,320,1120,320C1093.3,320,1067,320,1040,
+                320C1013.3,320,987,320,960,320C933.3,320,907,320,880,320C853.3,320,827,320,800,320C773.3,
+                320,747,320,720,320C693.3,320,667,320,640,320C613.3,320,587,320,560,320C533.3,320,507,320,
+                480,320C453.3,320,427,320,400,320C373.3,320,347,320,320,320C293.3,320,267,320,240,320C213.3,
+                320,187,320,160,320C133.3,320,107,320,80,320C53.3,320,27,320,13,320L0,320Z"></path>
+                </svg>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
